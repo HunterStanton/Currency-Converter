314d74d82bdd3a25f96ada3ca2967ce1c185e30e
Hunter Stanton <hunter_stanton@outlook.com>
2017-02-08 09:58:47 -0500

    Added Currency.swift

---
+++ Currency Converter/Currency.swift
@@ -0,0 +1,12 @@
+//
+//  Currency.swift
+//  Currency Converter
+//
+//  Created by Hunter Stanton on 2/8/17.
+//  Copyright © 2017 Hunter Stanton. All rights reserved.
+//
+//  The class that handles conversion of currency.
+//  This class will use SwiftyJSON to contact the Fixer.io API to get live, accurate conversion rates.
+//  In the future it will also store the conversion rates offline, so that Internet is not needed to use this application.
+
+import Foundation
