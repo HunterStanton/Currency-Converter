c9cb9ba499db0b7c1783604eb0bcc30016ccbd3e
Hunter Stanton <hunter_stanton@outlook.com>
2017-02-09 10:07:21 -0500

    Added Pods

---
+++ Pods/Target Support Files/Pods-Currency Converter/Pods-Currency Converter-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Currency_ConverterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Currency_ConverterVersionString[];
+
